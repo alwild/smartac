32fa537a331b79c4507525235aaf52b6afc0d616
Al Wild <albertwild@gmail.com>
2019-07-12 17:56:34 -0600

    will this ever work

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace smartacfe
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Azure!");
             });
         }
     }
