f578c8aac3c22f492cc19b6d1da6ee33e60cbe1c
Al Wild <albertwild@gmail.com>
2019-07-12 19:25:59 -0600

    added controllers and views

---
+++ Controllers/AdminPortalController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace smartacfe.Controllers
+{
+    public class AdminPortalController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ApiUsage()
+        {
+            return View();
+        }
+    }
+}
